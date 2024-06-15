648b078ad5fcaf461f944eb869fc1f9209670c33
angel zambrano <angeldzambrano99@gmail.com>
2024-06-15 16:56:52 -0700

    added stuff

--- world/src/App.jsx
+++ world/src/App.jsx
@@ -6,7 +6,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import HomePage from './pages/Homepage';
+import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
+import Login from './pages/Login';
 
 
 function App() {
@@ -17,6 +19,9 @@ function App() {
       <Route path='/' element={<HomePage />} /> 
       <Route path='product' element={<Product />} /> 
       <Route path='pricing' element={<Pricing />} /> 
+      <Route path='login' element={<Login />} /> 
+
+      <Route path='app' element={<AppLayout />} /> 
       <Route path='*' element={<PageNotFound />}/>
     </Routes>
 
