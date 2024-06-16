ebdb5807b91efde4847330b4b6489d2040b1f872
angel zambrano <angeldzambrano99@gmail.com>
2024-06-16 16:45:44 -0700

    added things

---
+++ world/src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import AppNav from './AppNav';
+import Logo from './Logo';
+import styles from './Sidebar.module.css';
+
+function Sidebar() {
+    return (<div className={styles.Sidebar}>
+        <Logo />
+        <AppNav />
+        <p>List of citirs</p>
+        <footer className={styles.footer}>
+            <p className={styles.copyright}>
+                &copy; Copyright {new Date().getFullYear()} by worldwise inc
+            </p>
+        </footer>
+    </div>);
+}
+
+export default Sidebar;
