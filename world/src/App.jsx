3ae8f271b6aba40c4045af09bfa884b93f910b73
angel zambrano <angeldzambrano99@gmail.com>
2024-06-17 01:33:12 -0700

    added a comonent

--- world/src/App.jsx
+++ world/src/App.jsx
@@ -9,6 +9,7 @@ import HomePage from './pages/Homepage';
 import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
 import Login from './pages/Login';
+import CityList from './components/CityList';
 
 
 function App() {
@@ -22,8 +23,8 @@ function App() {
       <Route path='login' element={<Login />} /> 
 
       <Route path='app' element={<AppLayout />} >
-          <Route index element={<p>List Of Cities</p>} />
-          <Route path='cities' element={<p>List of Cities</p>}/>
+          <Route index element={<CityList />} />
+          <Route path='cities' element={<CityList />}/>
           <Route path='countries' element={<p>List of Countries</p>}/>
           <Route path='form' element={<p>form</p>}/>
 
