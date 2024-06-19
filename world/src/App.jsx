fe5e40256e6b9dde66f4492af6691fefe824e5ea
angel zambrano <angeldzambrano99@gmail.com>
2024-06-19 12:37:33 -0700

    finished

--- world/src/App.jsx
+++ world/src/App.jsx
@@ -15,6 +15,7 @@ import City from './components/City';
 import Form from './components/Form';
 import { CitiesProvider } from './contexts/CitiesContext';
 import { AuthProvider } from './contexts/FakeAuthContext';
+import ProtectedRoute from './pages/protectedRoutes';
 
 
 
@@ -31,7 +32,11 @@ function App() {
       <Route path='pricing' element={<Pricing />} /> 
       <Route path='login' element={<Login />} /> 
 
-      <Route path='app' element={<AppLayout />} >
+      <Route path='app' element={
+      <ProtectedRoute>
+        <AppLayout />
+      </ProtectedRoute>
+      } >
           <Route index element={<Navigate replace to={"cities"} />} />
           <Route path='cities'
            element={<CityList />}/>
