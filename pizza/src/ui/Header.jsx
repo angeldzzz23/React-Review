92a5fcb05f4f77dafbcb76b0b76ef0da7b83465d
angel zambrano <angeldzambrano99@gmail.com>
2024-06-20 11:59:29 -0700

    feched the UI

---
+++ pizza/src/ui/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+
+    return (
+        <header>
+            <Link  to='/'>Pizza</Link>
+            <p>Angel</p>
+        </header>
+    );
+
+}
+
+export default Header;
