e074b61a0848ddefc324e7d417a10a0268b04722
angel zambrano <angeldzambrano99@gmail.com>
2024-06-22 14:56:38 -0700

    added styling

--- cabin/src/App.jsx
+++ cabin/src/App.jsx
@@ -1,15 +1,41 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import styled from 'styled-components'
+import GlobalStyles from './styles/GlobalStyles';
+import Input from './ui/Input';
+
+import Button from "./ui/Button";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight:600;
+  background-color:yellow;
+`;
+
+
+
+
+const StyledApp = styled.div`
+background-color:orangered;
+padding:20px;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-   <div>
-    Hello world
-   </div>
+    <>
+   <GlobalStyles />
+   <StyledApp>
+    <H1>The wild ones </H1>
+    <Button onClick={() => alert("check in")}> check in</Button>
+    <Button onClick={() => alert("check out")}>Check out</Button>
+
+    <Input type='number' placeholder='Number of guests' ></Input>
+    <Input type='number' placeholder='Number of guests' ></Input>
+
+   </StyledApp>
+   </>
   )
 }
 
