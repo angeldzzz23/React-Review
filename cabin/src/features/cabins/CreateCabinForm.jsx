89fcfcb923fe4035e850d6cf2bf60f6eaa54d296
angel zambrano <angeldzambrano99@gmail.com>
2024-07-29 02:45:43 -0700

    added cretion

--- cabin/src/features/cabins/CreateCabinForm.jsx
+++ cabin/src/features/cabins/CreateCabinForm.jsx
@@ -6,6 +6,9 @@ import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
 import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 // import { useFormAction } from "react-router-dom";
 
 
@@ -46,12 +49,25 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
+  const {register, handleSubmit, reset} = useForm();
 
-  const {register, handleSubmit} = useForm();
+
+  const queryClient = useQueryClient();
+
+
+  const {mutate, isLoading:isCreating } = useMutation({
+    mutationFn: createCabin, 
+    onSuccess: () => {
+      toast.success("new cabin successfully created");
+      queryClient.invalidateQueries({queryKey:["cabins"]});
+      reset();
+    },
+  })
+
 
 
   function onSubmit(data) {
-    console.log(data);
+    mutate(data);
   }
 
   return (
@@ -91,7 +107,7 @@ function CreateCabinForm() {
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button>Edit cabin</Button>
+        <Button disabled={isCreating}>Edit cabin</Button>
       </FormRow>
     </Form>
   );
